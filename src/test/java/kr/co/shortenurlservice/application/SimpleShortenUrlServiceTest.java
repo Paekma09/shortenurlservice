15c02de3ec786260f75025a97b28d3b3710eb637
KJ Nam <paekma09@gmail.com>
2024-03-11 23:45:13 +0900

    Initial commit

---
+++ src/test/java/kr/co/shortenurlservice/application/SimpleShortenUrlServiceTest.java
@@ -0,0 +1,33 @@
+package kr.co.shortenurlservice.application;
+
+import kr.co.shortenurlservice.presentation.ShortenUrlCreateRequestDto;
+import kr.co.shortenurlservice.presentation.ShortenUrlCreateResponseDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class SimpleShortenUrlServiceTest {
+
+    @Autowired
+    private SimpleShortenUrlService simpleShortenUrlService;
+
+    @Test
+    @DisplayName("URL을 단축한 후 단축된 URL 키로 조회하면 원래 URL이 조회되어야 한다.")
+    void shortenUrlAddTest() {
+        // given
+        String expectedOriginalUrl = "https://www.hanbit.co.kr";
+        ShortenUrlCreateRequestDto shortenUrlCreateRequestDto = new ShortenUrlCreateRequestDto(expectedOriginalUrl);
+
+        // when
+        ShortenUrlCreateResponseDto shortenUrlCreateResponseDto = simpleShortenUrlService.generateShortenUrl(shortenUrlCreateRequestDto);
+        String shortenUrlKey = shortenUrlCreateResponseDto.getShortenUrlKey();
+
+        // then
+        String originalUrl = simpleShortenUrlService.getOriginalUrlByShortenUrlKey(shortenUrlKey);
+        assertTrue(originalUrl.equals(expectedOriginalUrl));
+    }
+}
